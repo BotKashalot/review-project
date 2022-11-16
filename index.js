879f8a6e3d8dc5c03e7394ebb6c2b3751af8a344
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-16 16:47:40 -0500

    half hour left

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-const string = "tzetzo";
-const number = 8;
-const boolean = true;
 
-const array = [string, number, boolean];
+//const string = "tzetzo";
+//const number = 8;
+//const boolean = true;
+//
+//const array = [string, number, boolean];
+//
+//const object = {string, number, boolean};
+//const object2={
+//    stringValue: string,
+//    numberValue: number,
+//    booleanValue: boolean,
+//};
+//
+//console.log("logging object", object);
+//console.log((object));
+//console.log(object2);
+//
+//const roundNumber = Math.floor(Math.random() * 10);
+//
+//if (roundNumber >5){
+//    console.log("I am hungry");
+//} else {
+//    console.log("I am not hungry")
+//}
+//
+//const names = ["Tzetzo", "Elka", "Rubens"];
+//const vip = []
+//console.log(names[0])   // calls out "Tzetzo"
+//
+////for (i=0; i< names.length; i++){
+////    if(names[i] === "Rubens"){vip.push(names[i])}
+////}
+////``console.log(vip)
+//
+//const scores = {
+//    Tzetzo: 8,
+//    Elka: 9,
+//    Rubens: 4,
+//}
+//
+//console.log(scores["Tzetzo"])
+//console.log(scores.Tzetzo)
+//
+//console.log(Object.keys(scores))
+//console.log(Object.values(scores))
+//
+//for (i = 0; i < Object.keys(scores).length; i++){
+//    console.log(Object.values(scores)[i]);
+//    console.log(scores[Object.keys(scores)[i]])
+//    if (scores[Object.keys(scores)[i]] > 7){
+//        const key = Object.keys(scores)[i];
+//        const value = scores[Object.keys(scores)[i]];
+//        const object = {};
+//        object[key] = value;
+//        vip.push(object)
+//    }
+//}
+//console.log(vip)
 
-const object = {string, number, boolean};
-const object2={
-    stringValue: string,
-    numberValue: number,
-    booleanValue: boolean,
-};
-
-console.log("logging object", object);
-console.log((object));
-console.log(object2);
-
-const roundNumber = Math.floor(Math.random() * 10);
-
-if (roundNumber >5){
-    console.log("I am hungry");
-} else {
-    console.log("I am not hungry")
-}
-
-const names = ["Tzetzo", "Elka", "Rubens"];
-const vip = []
-console.log(names[0])   // calls out "Tzetzo"
-
-for (i=0; i< names.length; i++){
-    if(names[i] === "Rubens"){vip.push(names[i])}
-}
-
-console.log(vip)
-
-const scores = {
-    Tzetzo: 8,
-    Elka: 9,
-    Rubens: 4,
-}
-
-console.log(scores["Tzetzo"])
-console.log(scores.Tzetzo)
-
-console.log(Object.keys(scores))
-console.log(Object.values(scores))
-
-for (i = 0; i < Object.keys(scores).length; i++){
-    console.log(Object.values(scores)[i]);
-    console.log(scores[Object.keys(scores)[i]])
-}
+const image = document.querySelector("img")
+console.log(image)
